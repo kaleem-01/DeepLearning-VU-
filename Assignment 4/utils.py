322b9739bc29500ec2ec5f4865f225d0fe00a284
kaleem-01 <kaleemullahm@outlook.com>
2024-12-10 14:01:07 +0100

    Implementation for Assignment 4 Done

---
+++ Assignment 4/utils.py
@@ -0,0 +1,169 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+if torch.cuda.is_available():
+  device = 'cuda'
+else:
+  device = 'cpu'
+
+def evaluation(test_loader, name=None, model_best=None, epoch=None):
+    # EVALUATION
+    if model_best is None:
+        # load best performing model
+        model_best = torch.load(name + '.model')
+
+    model_best.eval()
+    loss = 0.
+    N = 0.
+    for indx_batch, (test_batch, _) in enumerate(test_loader):
+        test_batch = test_batch.to(device)
+        loss_t = model_best.forward(test_batch, reduction='sum')
+        loss = loss + loss_t.item()
+        N = N + test_batch.shape[0]
+    loss = loss / N
+
+    if epoch is None:
+        print(f'FINAL LOSS: nll={loss}')
+    else:
+        print(f'Epoch: {epoch}, val nll={loss}')
+
+    return loss
+
+
+def samples_real(name, test_loader, shape=(28,28)):
+    # real images-------
+    num_x = 4
+    num_y = 4
+    x, _ = next(iter(test_loader))
+    x = x.to('cpu').detach().numpy()
+
+    fig, ax = plt.subplots(num_x, num_y)
+    for i, ax in enumerate(ax.flatten()):
+        plottable_image = np.reshape(x[i], shape)
+        ax.imshow(plottable_image, cmap='gray')
+        ax.axis('off')
+
+    plt.savefig(name+'_real_images.pdf', bbox_inches='tight')
+    plt.close()
+
+
+def samples_generated(name, data_loader, shape=(28,28), extra_name=''):
+    x, _ = next(iter(data_loader))
+    x = x.to('cpu').detach().numpy()
+
+    # generations-------
+    model_best = torch.load(name + '.model')
+    model_best.eval()
+
+    num_x = 4
+    num_y = 4
+    x = model_best.sample(num_x * num_y)
+    x = x.to('cpu').detach().numpy()
+
+    fig, ax = plt.subplots(num_x, num_y)
+    for i, ax in enumerate(ax.flatten()):
+        plottable_image = np.reshape(x[i], shape)
+        ax.imshow(plottable_image, cmap='gray')
+        ax.axis('off')
+
+    plt.savefig(name + '_generated_images' + extra_name + '.pdf', bbox_inches='tight')
+    plt.close()
+
+
+def plot_curve(name, nll_val):
+    plt.plot(np.arange(len(nll_val)), nll_val, linewidth='3')
+    plt.xlabel('epochs')
+    plt.ylabel('nll')
+    plt.savefig(name + '_nll_val_curve.pdf', bbox_inches='tight')
+    plt.close()
+
+
+def training(name, max_patience, num_epochs, model, optimizer, training_loader, val_loader, shape=(28,28)):
+    nll_val = []
+    best_nll = 1000.
+    patience = 0
+
+    # Main loop
+    for e in range(num_epochs):
+        # TRAINING
+        model.train()
+        for indx_batch, (batch, _) in enumerate(training_loader):
+            batch = batch.to(device)
+            loss = model.forward(batch, reduction='mean')
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Validation
+        loss_val = evaluation(val_loader, model_best=model, epoch=e)
+        nll_val.append(loss_val)  # save for plotting
+
+        if e == 0:
+            print('saved!')
+            torch.save(model, name + '.model')
+            best_nll = loss_val
+        else:
+            if loss_val < best_nll:
+                print('saved!')
+                torch.save(model, name + '.model')
+                best_nll = loss_val
+                patience = 0
+
+                samples_generated(name, val_loader, shape=shape, extra_name="_epoch_" + str(e))
+            else:
+                patience = patience + 1
+
+        if patience > max_patience:
+            break
+
+    nll_val = np.asarray(nll_val)
+
+    return nll_val
+
+
+
+def log_categorical(x, p, num_classes=256, reduction=None, dim=None):
+    x_one_hot = F.one_hot(x.long(), num_classes=num_classes)
+    log_p = x_one_hot * torch.log(torch.clamp(p, EPS, 1. - EPS))
+    if reduction == 'avg':
+        return torch.mean(log_p, dim)
+    elif reduction == 'sum':
+        return torch.sum(log_p, dim)
+    else:
+        return log_p
+
+
+def log_bernoulli(x, p, reduction=None, dim=None):
+    pp = torch.clamp(p, EPS, 1. - EPS)
+    log_p = x * torch.log(pp) + (1. - x) * torch.log(1. - pp)
+    if reduction == 'avg':
+        return torch.mean(log_p, dim)
+    elif reduction == 'sum':
+        return torch.sum(log_p, dim)
+    else:
+        return log_p
+
+
+def log_normal_diag(x, mu, log_var, reduction=None, dim=None):
+    D = x.shape[1]
+    log_p = -0.5 * D * torch.log(2. * PI) - 0.5 * log_var - 0.5 * torch.exp(-log_var) * (x - mu)**2.
+    if reduction == 'avg':
+        return torch.mean(log_p, dim)
+    elif reduction == 'sum':
+        return torch.sum(log_p, dim)
+    else:
+        return log_p
+
+
+def log_standard_normal(x, reduction=None, dim=None):
+    D = x.shape[1]
+    log_p = -0.5 * D * torch.log(2. * PI) - 0.5 * x**2.
+    if reduction == 'avg':
+        return torch.mean(log_p, dim)
+    elif reduction == 'sum':
+        return torch.sum(log_p, dim)
+    else:
+        return log_p
+
